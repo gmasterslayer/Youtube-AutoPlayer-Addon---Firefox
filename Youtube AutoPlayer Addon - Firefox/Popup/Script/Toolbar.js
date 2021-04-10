04064989e6fef9fc97cd98c73c39749de3869508
gmasterslayer <gmasterslayer@gmail.com>
2021-04-10 17:24:41 +0900

    Version 1.2.1

---
+++ Youtube AutoPlayer Addon - Firefox/Popup/Script/Toolbar.js
@@ -0,0 +1,22 @@
+/* Retrieve any previously set cookie and send to content script */
+// get any previously set cookie for the current tab 
+function cookieUpdate() {
+  let gettingCookies = browser.cookies.get({
+    url: "https://www.youtube.com/",
+    name: "YouTube_AutoPlayer"
+  });
+  gettingCookies.then((cookie) => {
+    if (cookie) {
+      let p = document.getElementById('autoplayed');
+      if (parseInt(cookie.value) == 0) {
+        var smilely = '<p style="text-align:center"> :(</p>';
+      } else {
+        var smilely = '<p style="text-align:center"> :)</p>';
+      }
+      p.innerHTML = "Youtube Autoplayer has autoplayed " + cookie.value + " times" + smilely;
+    } else {
+      p.innerHTML = "No previous autorun instances";
+    }
+  });
+}
+setInterval(cookieUpdate, 500);
---
+++ Youtube AutoPlayer Addon - Firefox/Script/Youtube_Autoplay.js
@@ -0,0 +1,75 @@
+var delay = 1500;
+var ran = false;
+let debug = false;
+let reset = false;
+var autoplays = 0;
+if (debug){
+	alert("No syntax errors. Injection successful");
+}
+if (getCookie("YouTube_AutoPlayer") == null){
+	setCookie("YouTube_AutoPlayer", 0, 999); //creates cookie with 0 autoplays saved
+	if (debug){
+		alert("Cookie Set");
+	}
+} else { //reads the cookies and recovers the statistics value for autoplays
+	if (reset){
+		setCookie("YouTube_AutoPlayer", 0, 999);
+	}
+	autoplays = parseInt(getCookie("YouTube_AutoPlayer")); //convert the returning string into integer. broke shit last time
+	if (debug){
+		alert("Cookie found and variables set. Record found " + autoplays + " autoplays.");
+		
+	}
+}
+var everythingLoaded = setInterval(function() { //checks the dom if everything is loaded
+//could be depricated with the use of the manifest
+  if (/loaded|complete/.test(document.readyState)) {
+    clearInterval(everythingLoaded);
+    var title = document.getElementsByTagName("TITLE")[0];
+    title = title.innerHTML;
+    if (title.search("YouTube") != -1){
+            var test = setInterval(Checker, delay);
+			cookieUpdate();
+            //setTimeout(repeat, time);
+    }
+  }
+}, 1000);
+function Checker() {
+	if (ran != true){
+		ran = true;
+		console.log("YouTube_AutoPlayer Status: Working");
+	}
+    let arr = Array.from(document.querySelectorAll('.ytd-popup-container'));
+    var elem = arr[1]; // the popup
+    if(elem != null | undefined) {
+        document.getElementById('text').click();
+		console.log("popup clicked");
+		//elem.click(); 
+		autoplays += 1;
+		setCookie("YouTube_AutoPlayer", autoplays, 999);
+		elem = undefined; // unset
+		//delete(elem); // this removes the variable completely | doesnt seem to be working
+    }
+ }
+function setCookie(c_name, value, expiredays) {  //sets cookie for statistics popup
+    var exdate = new Date();  
+    exdate.setDate(exdate.getDate() + expiredays);  
+    document.cookie = c_name + "=" + value + ";path=/" + ((expiredays ==null) ? "" : ";expires=" + exdate.toGMTString());  
+    } 
+function getCookie(name) {  //reads statisics from cookie
+    var dc = document.cookie;  
+    var prefix = name +"=";  
+    var begin = dc.indexOf("; " + prefix);  
+    if (begin == -1) {  
+        begin = dc.indexOf(prefix);  
+        if (begin != 0)return null;  //no statistics saved
+    } else {  
+        begin += 2;  
+    }  
+    var end = document.cookie.indexOf(";", begin);  
+    if (end == -1) {  
+        end = dc.length;  
+    }  
+    return unescape(dc.substring(begin + prefix.length, end));  
+} 
+//------------------------------Mark start of experimental json--------------------------------
