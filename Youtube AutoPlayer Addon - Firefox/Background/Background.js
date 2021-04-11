6518a4c72d6ebbee97c1a54638d5084771bb3c38
gmasterslayer <gmasterslayer@gmail.com>
2021-04-11 12:45:55 +0900

    Version 1.2.1
    cosmetic update to the code

--- Youtube AutoPlayer Addon - Firefox/Background/Background.js
+++ Youtube AutoPlayer Addon - Firefox/Background/Background.js
@@ -1,24 +1,27 @@
 /* Retrieve any previously set cookie and send to content script */
 
 function getActiveTab() {
-  return browser.tabs.query({active: true, currentWindow: true});
+    return browser.tabs.query({
+        active: true,
+        currentWindow: true
+    });
 }
 
 function cookieUpdate() {
-  getActiveTab().then((tabs) => {
-    // get any previously set cookie for the current tab 
-    var gettingCookies = browser.cookies.get({
-      url: tabs[0].url,
-      name: "YouTube_AutoPlayer"
+    getActiveTab().then((tabs) => {
+        // get any previously set cookie for the current tab 
+        var gettingCookies = browser.cookies.get({
+            url: tabs[0].url,
+            name: "YouTube_AutoPlayer"
+        });
+        gettingCookies.then((cookie) => {
+            if (cookie) {
+                //var cookieVal = JSON.parse(cookie.value);
+                browser.tabs.sendMessage(tabs[0].id, cookie.value);
+                //browser.tabs.sendMessage(tabs[0].id, {color: cookieVal.color});
+            }
+        });
     });
-    gettingCookies.then((cookie) => {
-      if (cookie) {
-        //var cookieVal = JSON.parse(cookie.value);
-        browser.tabs.sendMessage(tabs[0].id, cookie.value);
-        //browser.tabs.sendMessage(tabs[0].id, {color: cookieVal.color});
-      }
-    });
-  }); 
 }
 setInterval(cookieUpdate, 5000);
 // update when the tab is updated
